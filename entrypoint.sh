165d31ec506d45db6d57235e288e8e9c9e94735e
Georgi Todorov <51453974+GVLTodorov@users.noreply.github.com>
2022-02-23 20:23:56 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Create Directories..."
+mkdir /etc/periodic
+mkdir /etc/periodic/1min
+mkdir /etc/periodic/15min
+mkdir /etc/periodic/30min
+mkdir /etc/periodic/hourly
+mkdir /etc/periodic/12hour
+mkdir /etc/periodic/daily
+mkdir /etc/periodic/weekly
+mkdir /etc/periodic/monthly
