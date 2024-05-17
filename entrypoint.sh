43341725fb6c936cd7a9a7c59072c1334d479232
Georgi Todorov <51453974+GVLTodorov@users.noreply.github.com>
2024-05-17 18:53:09 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo "Create Directories..."
-mkdir -p /etc/cronner/1min
-mkdir -p /etc/cronner/15min
-mkdir -p /etc/cronner/30min
-mkdir -p /etc/cronner/hourly
-mkdir -p /etc/cronner/12hour
-mkdir -p /etc/cronner/daily
-mkdir -p /etc/cronner/weekly
-mkdir -p /etc/cronner/monthly
+mkdir -p /srv/cronner/1min
+mkdir -p /srv/cronner/15min
+mkdir -p /srv/cronner/30min
+mkdir -p /srv/cronner/hourly
+mkdir -p /srv/cronner/12hour
+mkdir -p /srv/cronner/daily
+mkdir -p /srv/cronner/weekly
+mkdir -p /srv/cronner/monthly
 echo "End..."
